9ac14a765530a1ce708ba50bf75153a13c056206
Saji Champlin <kschamplin19@blakeschool.org>
2017-02-03 17:52:58 -0600

    corelibs and stuff

--- SDVX_ASC.ino
+++ SDVX_ASC.ino
@@ -32,13 +32,14 @@ typedef struct {
 #define SWITCH_COUNT 7
 
 switch_t switches[SWITCH_COUNT] = {
-  {1, 5, 7}, // START
-  {23, 7, 1}, // A
-  {20, 9, 2}, // B
-  {10, 16, 3}, // C
-  {14, 15, 4}, // D
-  {16, 17, 5}, // FX A
-  {18, 19, 6}, // FX B
+  {4, 12, 1}, // A
+  {5, 14, 2}, // B
+  {6, 15, 3}, // C
+  {7, 16, 4}, // D
+  {8, 17, 5}, // FX A
+  {9, 18, 6}, // FX B
+  {10, 19, 7}, // START
+  {11, 0, 8} // TEST (no led)
 };
 
 void setup()
@@ -49,23 +50,28 @@ void setup()
   }
   pinMode(5, OUTPUT);
   Serial.begin(57600);
+  Arcade.useManualSend(true); // lets reduce the calls
 }
 byte buffer[2];
 
 void loop() {
-  int n = Arcade.recv(buffer, 15); // no timeout
+  int n = Arcade.recv(buffer, 0); // 0 timeout = do not wait
+  if (n > 0) {
+    // the computer sent a message.  Display the bits
+    // of the first byte on pin 0 to 7.  Ignore the
+    // other 63 bytes!
+    Serial.print(F("Received packet, first byte: "));
+    Serial.println((int)buffer[0]);
+    for (int i=0; i<8; i++) {
+      int b = buffer[0] & (1 << i);
+      digitalWrite(i, b);
+    }
+  }
 
   if (n > 0) {
     Serial.print("recieved: ");
     Serial.print(n);
     Serial.print((uint32_t) buffer);
-  }
-  for (int i = 0; i < SWITCH_COUNT; i++) {
-    if (digitalRead(switches[i].switchPin) == LOW) {
-      Arcade.button(switches[i].joyId, 1);
-    } else {
-      Arcade.button(switches[i].joyId, 0);
-    }
   }
   for (int i = 0; i < ENCODER_COUNT; i++) {
     long encVal = encoders[i].enc.read();
@@ -78,5 +84,11 @@ void loop() {
       encoders[i].enc.write(0);
     }
   }
+  ButtonChange(1);
   delay(DELAY);
+}
+
+void ButtonChange(uint id) {
+  usb_arcade_data[0] ^= 1 << id; // toggle the state of the button id
+  Arcade.send_now(); // the state changed so we send it.
 }
---
+++ corelib/readme.txt
@@ -0,0 +1,3 @@
+These files should replace or be added to the teensy library
+
+TODO: use diffs instead of whole files?
